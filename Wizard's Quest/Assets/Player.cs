ba734238ecd946f4053baf17a4d9329d7b03fed2
Peter Ferri <pjferri10@gmail.com>
2022-10-04 00:27:54 -0400

    Movement! (Sort of)
    After struggling for a while I now finally have some movement for the wizard. Press up to move towards the mouse, and back to move away.

--- Wizard's Quest/Assets/Player.cs
+++ Wizard's Quest/Assets/Player.cs
@@ -5,18 +5,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int offset;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         faceMouse();
+        movePlayer();
+    }
+
+    private void movePlayer()
+    {
+        Vector3 mousePosition = Input.mousePosition; //Get the mouse position and save it
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition); //Covert the mouse position from screenspace to world space
+
+        Vector2 direction = new Vector2(transform.position.x - mousePosition.x, transform.position.y - mousePosition.y);
+        //copied is above
+
+        if (Input.GetAxis("Vertical") > 0) //The value moves between -1 and +1 depending on which button is pressed. Up is positive, Down is negative, Zero is no movement
+        {
+            transform.Translate(Vector3.down * Time.deltaTime);
+        }
+
+        //Do the opposite down here with another if
+        if (Input.GetAxis("Vertical") < 0) //The value moves between -1 and +1 depending on which button is pressed. Up is positive, Down is negative, Zero is no movement
+        {
+            transform.Translate(Vector3.up * Time.deltaTime);
+        }
     }
 
     private void faceMouse()
